d6ef524baa067afade277c1f51a0ba967d8b4cfc
walterlbf <walter.lbf.f@gmail.com>
2021-09-24 00:48:18 -0400

    bora começar

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Código que será executado quando o navegador carregar
+
 });
+
+async function fetchMealsRecomendation() {
+  const request = await fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic');
+  const response = await request.json();
+  console.log(response);
+  return response;
+}
+
+fetchMealsRecomendation();
