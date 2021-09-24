fc75a43be496fca76cc7ee1f1a97992b144e3b10
walterlbf <walter.lbf.f@gmail.com>
2021-09-24 02:08:53 -0400

    imagens renderizando na tela

--- scripts.js
+++ scripts.js
@@ -3,11 +3,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-async function fetchMealsRecomendation() {
+async function fetchPhots() {
   const request = await fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic');
   const response = await request.json();
-  console.log(response);
   return response;
+};
+
+const appendItemList = (height, width, url, link) => {
+
+  const newImg = document.createElement('img');
+  document.querySelector('div').appendChild(newImg);
+
+  // console.log(url);
+  newImg.src = url;
+  newImg.width = width;
+  newImg.height = height;
 }
 
-fetchMealsRecomendation();
+const mapPhotos = (getPhotos) => {
+  getPhotos.map(({ link, imagens: { resolucaoMedia: { height, width, url } } }) => {
+    appendItemList(height, width, url, link);
+  });
+};
+
+window.onload = async function () {
+  const getPhotos = await fetchPhots();
+  await mapPhotos(getPhotos);
+};
